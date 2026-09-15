4056e522c0939cf10173a1fe51029d7b03004df3
Kabir Alkasim <codesmartng1@gmail.com>
2026-09-15 07:12:55 +0100

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -41,6 +41,7 @@ app.use(cors({
     'http://localhost:5173',
     'http://localhost:3000',
     'http://127.0.0.1:5173',
+    'https://code-stem-education-system-i5dv.vercel.app',
     'https://code-stem-education-system-one.vercel.app'
   ],
   credentials: true,
